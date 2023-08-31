985ab0707a1d0942e5bc87dfb9a29ed0b8c2fdc1
unamiatoii <pierrejaphetkonan@gmail.com>
2023-08-31 15:56:17 +0000

    Installation de firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wesika/pages/Acceuil/OnBoard.dart';
 import 'package:wesika/pages/mainPage/HomePage.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
