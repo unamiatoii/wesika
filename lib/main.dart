2c5ac7fc7cdca8c8f3da4dc69b0bb26163bf30ab
unamiatoii <pierrejaphetkonan@gmail.com>
2023-09-04 01:39:21 +0000

    Revert "Installation de firebase"
    This reverts commit 985ab0707a1d0942e5bc87dfb9a29ed0b8c2fdc1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wesika/pages/Acceuil/OnBoard.dart';
 import 'package:wesika/pages/mainPage/HomePage.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+void main() {
   runApp(const MyApp());
 }
 
