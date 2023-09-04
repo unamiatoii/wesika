ddd4212d96c35c95a71b7f8fd6b7ecad28e14a1e
unamiatoii <pierrejaphetkonan@gmail.com>
2023-09-04 01:39:27 +0000

    Auth ok avec firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:wesika/pages/Acceuil/OnBoard.dart';
 import 'package:wesika/pages/mainPage/HomePage.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
